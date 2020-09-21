8dc99fe9777b54edd002a608fd7efe744a720a0e
Devansh Shah <devzs6.98@gmail.com>
2020-09-21 16:21:04 -0400

    fromatting

--- src/index.js
+++ src/index.js
@@ -5,20 +5,25 @@ const fetch = require("node-fetch");
 const chalk = require("chalk");
 const link_reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
 
-const StatusEnum = Object.freeze({good:'GOOD',bad:'BAD',unknown:'UNKNOWN'}); // create a ststus enumeration
+const StatusEnum = Object.freeze({
+  good: "GOOD",
+  bad: "BAD",
+  unknown: "UNKNOWN",
+}); // create a status enumeration
 
-class Link { // class for Link with 2 propties Url and status 
+class Link {
+  // class for Link with 2 propties Url and status
   constructor(url, status) {
     this.url = url;
     this.status = status;
   }
   toString() {
-      return `Link: ${this.url} status : ${this.status}`;
+    return `Link: ${this.url} status : ${this.status}`;
   }
 }
 
-
-const linkchecker = async (link) => { //processes Links
+const linkchecker = async (link) => {
+  //processes Links
   let status = 200;
   try {
     let res = await fetch(link.url);
@@ -43,28 +48,34 @@ const linkchecker = async (link) => { //processes Links
   }
 };
 
-const documentProccessing = async (doc) => { //processDocument
+const documentProccessing = async (doc) => {
+  //processDocument
   try {
     let data = await fs.readFile(process.cwd() + "/" + doc, "utf8"); // gets the data in the document
     doc = data.toString(); // converts it to a string
-  
+
     //LINK PROCESSING
     let links = [];
-      new Set(doc.match(link_reg)).forEach(e => { // gets all http/https linnks in the document and create a Link and checks it
-      let checkThis = new Link(e,StatusEnum.unknown);
+
+    new Set(doc.match(link_reg)).forEach((e) => {
+      // gets all http/https linnks in the document and create a Link and checks it
+
+      let checkThis = new Link(e, StatusEnum.unknown);
       linkchecker(checkThis);
       links.push(checkThis);
     });
-  } catch (e) { console.log(e)}
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const argv = yargs
   .usage(
     "To use this tool type :\n$flink <file> -- where file is the name of the file"
   )
-  .alias('v','version')
-  .help('h')
-  .alias('h', 'help')
+  .alias("v", "version")
+  .help("h")
+  .alias("h", "help")
   .demandCommand(1, "").argv;
 let document = "";
 
